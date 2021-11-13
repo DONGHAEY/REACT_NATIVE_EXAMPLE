d446addffdec4e73e71826559b26d952d346f3a6
DONGHAEY <80025157+DONGHAEY@users.noreply.github.com>
2021-11-13 23:12:18 +0900

    I used if in TEXT JSX

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const name = "Hanbit";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        {(() => {
+          if (name === "Hanbit") return "Myname is Habit";
+          else if (name === "Beomjun") return "Myname is Beomjun";
+          else return "Myname is ReactNative";
+        })()}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
